ef3410161ce448361f091dbc0c156b8815c2c31f
Jesus Macias <maciasfelixj@gmail.com>
2020-10-04 13:15:16 +0200

    Install react modal and create a modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { UserChat } from "./components";
+import Modal from "react-modal";
 import "./App.css";
+
+Modal.setAppElement(document.getElementById("root"));
 
 const App = () => {
   const users = [];
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [userChat, setUserChat] = React.useState("");
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const createUserChat = (e) => {
+    e.preventDefault();
+    users.push(userChat);
+    console.log(users.length)
+    setUserChat("");
+    setIsOpen(false);
+  };
+
   return (
     <>
       <div className="nav">
@@ -17,7 +35,27 @@ const App = () => {
             🦈
           </span>{" "}
         </div>
-        <button className="nav-btn">+</button>
+        <button onClick={openModal} className="nav-btn">
+          +
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          contentLabel="Example Modal"
+          className="modal-style"
+        >
+          <div>
+            <h2>Create a User</h2>
+            <form onSubmit={createUserChat}>
+              <input
+                type="text"
+                placeholder="Username..."
+                value={userChat}
+                onChange={(e) => setUserChat(e.target.value)}
+              />
+              <button>Set user</button>
+            </form>
+          </div>
+        </Modal>
       </div>
       {users.length ? (
         <div className="chat-grid">
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { UserChat } from "./UserChat";
 export { default as SecondPerson } from "./SecondPerson";
-export { default as PersonSwitcher } from "./PersonSwitcher";
