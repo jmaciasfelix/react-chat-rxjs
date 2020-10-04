c2e92b7a716f3a589385ee5475b2a0e2606a9d7e
Jesus Macias <maciasfelixj@gmail.com>
2020-10-04 12:40:13 +0200

    Add inform

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import React from "react";
 import { UserChat } from "./components";
 import "./App.css";
 
-const App = () => (
-  <>
-    <div className="nav">
-      <div className="nav-title">
-        Chat React{" "}
-        <span role="img" aria-label="shark-emoji">
-          ⚛️
-        </span>{" "}
-        and RxJS{" "}
-        <span role="img" aria-label="shark-emoji">
-          🦈
-        </span>{" "}
+const App = () => {
+  const users = [];
+  return (
+    <>
+      <div className="nav">
+        <div className="nav-title">
+          Chat React{" "}
+          <span role="img" aria-label="shark-emoji">
+            ⚛️
+          </span>{" "}
+          and RxJS{" "}
+          <span role="img" aria-label="shark-emoji">
+            🦈
+          </span>{" "}
+        </div>
+        <button className="nav-btn">+</button>
       </div>
-      <button className="nav-btn">+</button>
-    </div>
-    <div className="chat-grid">
-      <UserChat nameUser="JesusMf" />
-      <UserChat nameUser="Alvaro" />
-      <UserChat nameUser="Alvaro" />
-    </div>
-  </>
-);
+      {users.length ? (
+        <div className="chat-grid">
+          <UserChat nameUser="JesusMf" />
+          <UserChat nameUser="Alvaro" />
+          <UserChat nameUser="Alvaro" />
+        </div>
+      ) : (
+        <h1 className="inform">Create new users to test the chat</h1>
+      )}
+    </>
+  );
+};
 
 export default App;
